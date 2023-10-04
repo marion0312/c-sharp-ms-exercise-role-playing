695e18bb7a7d6e0df4c91ea4a1658553c22d9007
Marion Balba <balba.marion@gmail.com>
2023-10-04 08:56:38 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿var heroHP = 10;
+var monsterHP = 10;
+var currentTurn = "hero";
+Random randomAtk = new();
+var dmg = 0;
+
+do {
+    dmg = randomAtk.Next(1, 11);
+    if (currentTurn == "hero") {
+        monsterHP -= dmg;
+        System.Console.WriteLine($"Monster was damaged and lost {dmg} health and now has {monsterHP} health.");
+        currentTurn = "monster";
+    } else {
+        heroHP -= dmg;
+        currentTurn = "hero";
+        System.Console.WriteLine($"Hero was damaged and lost {dmg} health and now has {heroHP} health.");
+    }
+
+} while ( heroHP >= 0 && monsterHP >= 0 );
+
+if (heroHP <= 0) 
+    System.Console.WriteLine("Monster wins!");
+else
+    System.Console.WriteLine("Hero wins!");
